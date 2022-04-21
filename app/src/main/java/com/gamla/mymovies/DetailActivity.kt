c0e66d584a0b063d99dd99827b156efa9c8dc61c
Boris <borissoto@gmail.com>
2022-04-20 20:48:00 -0400

    add navigation to detail activity

--- app/src/main/java/com/gamla/mymovies/DetailActivity.kt
+++ app/src/main/java/com/gamla/mymovies/DetailActivity.kt
@@ -2,6 +2,9 @@ package com.gamla.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
 import com.bumptech.glide.Glide
 import com.gamla.mymovies.databinding.ActivityDetailBinding
 import com.gamla.mymovies.model.Movie
@@ -20,10 +23,32 @@ class DetailActivity : AppCompatActivity() {
         val movie = intent.getParcelableExtra<Movie>(EXTRA_MOVIE)
 
         if (movie != null) {
-            binding.title1.text = movie.title
+            title = movie.title
             Glide
                 .with(this)
-                .load("https://image.tmdb.org/t/p/w780/${movie.backdrop_path}").into(binding.backdrop)
+                .load("https://image.tmdb.org/t/p/w780/${movie.backdrop_path}")
+                .into(binding.backdrop)
+            binding.summary.text = movie.overview
+            bindDetailInfo(binding.detailInfo, movie)
+        }
+    }
+
+    private fun bindDetailInfo(detailInfo: TextView, movie: Movie) {
+        detailInfo.text = buildSpannedString {
+            bold { append("Original language: ") }
+            appendLine(movie.original_language)
+
+            bold { append("Original title: ") }
+            appendLine(movie.original_title)
+
+            bold { append("Release date: ") }
+            appendLine(movie.release_date)
+
+            bold { append("Popularity: ") }
+            appendLine(movie.popularity.toString())
+
+            bold { append("Vote average: ") }
+            appendLine(movie.vote_average.toString())
         }
     }
 }
