4473bda7b234355eacda1043c266330a82648ca5
Boris Soto <borissoto@gmail.com>
2022-05-01 20:40:35 -0400

    add permission and request location (#12)
    permissions

--- app/src/main/java/com/gamla/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/gamla/mymovies/ui/main/MainActivity.kt
@@ -1,33 +1,88 @@
 package com.gamla.mymovies.ui.main
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.icu.util.TimeZone.getRegion
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
 import com.gamla.mymovies.R
 import com.gamla.mymovies.databinding.ActivityMainBinding
 import com.gamla.mymovies.model.Movie
 import com.gamla.mymovies.model.MovieDbClient
 import com.gamla.mymovies.ui.detail.DetailActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DEFAULT_REGION = "US"
+    }
+
+    private val moviesAdapter = MoviesAdapter(emptyList()) { navigateTo(it) }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            doRequestPopularMovies(isGranted)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val moviesAdapter = MoviesAdapter(emptyList()) {
-            navigateTo(it)
-        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         binding.recycler.adapter = moviesAdapter
 
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    private fun doRequestPopularMovies(isLocationGranted: Boolean) {
         lifecycleScope.launch {
             val apiKey = getString(R.string.api_key)
-            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey)
+            val region = getRegion(isLocationGranted)
+            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey, region)
             moviesAdapter.movies = popularMovies.results
             moviesAdapter.notifyDataSetChanged()
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun getRegion(isLocationGranted: Boolean): String = suspendCancellableCoroutine{ continuation ->
+        if (isLocationGranted) {
+            fusedLocationClient.lastLocation.addOnCompleteListener {
+                continuation.resume(getRegionFromLocation(it.result))
+            }
+        } else {
+            continuation.resume(DEFAULT_REGION)
+        }
+    }
+
+    private fun getRegionFromLocation(location: Location?): String {
+        if (location == null){
+            return DEFAULT_REGION
+        }
+
+        val geocoder = Geocoder(this)
+        val result = geocoder.getFromLocation(
+            location.latitude,
+            location.longitude,
+            1
+        )
+        return result.firstOrNull()?.countryCode ?: DEFAULT_REGION
     }
 
     private fun navigateTo(movie: Movie) {
