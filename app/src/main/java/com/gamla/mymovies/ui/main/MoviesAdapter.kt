4112eb1c7e5a51b3d6e178975855ad644dfdf1bb
Boris <borissoto@gmail.com>
2022-05-12 12:12:14 -0400

    extract DiffUtil from adapter

--- app/src/main/java/com/gamla/mymovies/ui/main/MoviesAdapter.kt
+++ app/src/main/java/com/gamla/mymovies/ui/main/MoviesAdapter.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.gamla.mymovies.R
 import com.gamla.mymovies.databinding.ViewMovieItemBinding
 import com.gamla.mymovies.model.Movie
+import com.gamla.mymovies.ui.common.basicDiffUtil
 import com.gamla.mymovies.ui.common.inflate
 import com.gamla.mymovies.ui.common.loadUrl
 import kotlin.properties.Delegates
@@ -16,29 +17,20 @@ import kotlin.properties.Delegates
 class MoviesAdapter(private val listener: (Movie) -> Unit) :
     RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
-    var movies: List<Movie> by Delegates.observable(emptyList()) { _, old, new->
-        DiffUtil.calculateDiff(object : DiffUtil.Callback(){
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                old[oldItemPosition].id == new[newItemPosition].id
-
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                old[oldItemPosition] == new[newItemPosition]
-
-            override fun getOldListSize(): Int = old.size
-
-            override fun getNewListSize(): Int = new.size
-        }).dispatchUpdatesTo(this)
-    }
+    var movies: List<Movie> by basicDiffUtil(
+        emptyList(),
+        areItemsTheSame = { old, new -> old.id == new.id }
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = parent.inflate(
-            R.layout.view_movie_item,false
+            R.layout.view_movie_item, false
         )
 
         return ViewHolder(view)
     }
 
-    override fun getItemCount() = movies.size
+    override fun getItemCount(): Int = movies.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = movies[position]
