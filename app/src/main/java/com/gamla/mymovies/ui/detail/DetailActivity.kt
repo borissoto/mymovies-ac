a2b46476fc0225bde41bb396723c7beb6b72855b
Boris Soto <borissoto@gmail.com>
2022-05-14 12:40:57 -0400

    Feature/mvvm  (#20)
    * main viewmodel
    
    * add viewmodel to detail

--- app/src/main/java/com/gamla/mymovies/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/gamla/mymovies/ui/detail/DetailActivity.kt
@@ -3,49 +3,38 @@ package com.gamla.mymovies.ui.detail
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.widget.TextView
-import androidx.core.text.bold
-import androidx.core.text.buildSpannedString
-import com.bumptech.glide.Glide
-import com.gamla.mymovies.R
+import androidx.activity.viewModels
 import com.gamla.mymovies.databinding.ActivityDetailBinding
 import com.gamla.mymovies.model.Movie
 import com.gamla.mymovies.ui.common.loadUrl
 
 
-class DetailActivity : AppCompatActivity(), DetailPresenter.View {
-
-    private val presenter = DetailPresenter()
-    private lateinit var binding: ActivityDetailBinding
-
+class DetailActivity : AppCompatActivity() {
     companion object {
         const val MOVIE = "DetailActivity:movie"
     }
 
-    @SuppressLint("SetTextI18n")
+    private val viewModel: DetailViewModel by viewModels {
+        DetailViewModelFactory(requireNotNull(intent.getParcelableExtra(MOVIE)))
+    }
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityDetailBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        setContentView(binding.root)
 
-            val movie: Movie = requireNotNull(intent.getParcelableExtra(MOVIE))
-            presenter.onCreate(this, movie)
+        viewModel.state.observe(this) { updateUi(it.movie)}
     }
 
-    override fun onDestroy() {
-        presenter.onDestroy()
-        super.onDestroy()
-    }
-
-    override fun updateUI(movie: Movie) = with(binding) {
+    private fun updateUi(movie: Movie) = with(binding) {
         setSupportActionBar(binding.movieDetailToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         movieDetailToolbar.title = movie.title
 
-        val background = movie.backdropPath ?: movie.posterPath
-        movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780/${background}")
+        movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780/${movie.backdropPath}")
         movieDetailSummary.text = movie.overview
         movieDetailInfo.setMovie(movie)
         fab.setOnClickListener {
--- app/src/main/java/com/gamla/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/gamla/mymovies/ui/main/MainActivity.kt
@@ -1,32 +1,20 @@
 package com.gamla.mymovies.ui.main
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.location.Geocoder
-import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import com.gamla.mymovies.R
+import androidx.activity.viewModels
 import com.gamla.mymovies.databinding.ActivityMainBinding
 import com.gamla.mymovies.model.Movie
 import com.gamla.mymovies.model.MoviesRepository
-import com.gamla.mymovies.model.RemoteConnection
+import com.gamla.mymovies.ui.common.visible
 import com.gamla.mymovies.ui.detail.DetailActivity
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity() {
 
-    private val presenter by lazy { MainPresenter(MoviesRepository(this), lifecycleScope) }
-    private val moviesAdapter = MoviesAdapter { presenter.onMovieClicked(it) }
+    private val viewModel: MainViewModel by viewModels { MainViewModelFactory(MoviesRepository(this)) }
+
+    private val moviesAdapter = MoviesAdapter { viewModel.onMovieClicked(it) }
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,27 +23,17 @@ class MainActivity : AppCompatActivity(), MainPresenter.View {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.recycler.adapter = moviesAdapter
-        presenter.onCreate(this)
+
+        viewModel.state.observe(this, ::updateUi)
     }
 
-    override fun onDestroy() {
-        presenter.onDestroy()
-        super.onDestroy()
+    private fun updateUi(state: MainViewModel.UiState) {
+        binding.progressBar.visible = state.loading
+        state.movies?.let(moviesAdapter::submitList)
+        state.navigateTo?.let(::navigateTo)
     }
 
-    override fun showProgress() {
-        binding.progressBar.visibility = View.VISIBLE
-    }
-
-    override fun updateData(movies: List<Movie>) {
-        moviesAdapter.submitList(movies)
-    }
-
-    override fun hideProgress() {
-        binding.progressBar.visibility = View.GONE
-    }
-
-    override fun navigateTo(movie: Movie) {
+    private fun navigateTo(movie: Movie) {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.MOVIE, movie)
         startActivity(intent)
